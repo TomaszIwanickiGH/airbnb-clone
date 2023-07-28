84ebd3e446dd3c3c107aa957987c612b5502fa5f
Tomasz <tomasz.iwanicki@onet.pl>
2023-07-28 20:12:37 +0200

    Wrap Navbar with ClientOnly component

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,9 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Nunito } from 'next/font/google'
 import Navbar from './components/navbar/Navbar'
+import ClientOnly from './components/ClientOnly'
+import RegisterModal from './components/modals/RegisterModal'
+import ToasterProvider from './providers/ToasterProvider'
 
 const font = Nunito({
   subsets: ['latin'],
@@ -16,7 +19,11 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <body className={font.className}>
-        <Navbar />
+        <ClientOnly>
+          <ToasterProvider />
+          <RegisterModal />
+          <Navbar />
+        </ClientOnly>
         {children}
       </body>
     </html>
