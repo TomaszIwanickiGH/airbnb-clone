d6243715dbc5f04d81c4f8947798695e9eebd987
Tomasz <tomasz.iwanicki@onet.pl>
2023-08-03 19:24:36 +0200

    Add Categories component to the navbar

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import Container from '../Container'
+import Categories from './Categories'
 import Logo from './Logo'
 import Search from './Search'
 import UserMenu from './UserMenu'
@@ -10,7 +11,7 @@ interface NavbarProps {
   currentUser?: SafeUser | null
 }
 
-const Navbar: React.FC<NavbarProps> = ({currentUser}) => {
+const Navbar: React.FC<NavbarProps> = ({ currentUser }) => {
   return (
     <div className="fixed w-full bg-white z-10 shadow-sm">
       <div className="py-4 border-b-[1px]">
@@ -22,6 +23,7 @@ const Navbar: React.FC<NavbarProps> = ({currentUser}) => {
           </div>
         </Container>
       </div>
+      <Categories />
     </div>
   )
 }
