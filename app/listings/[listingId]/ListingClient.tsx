15e52e45c1326c2a25d2b77a53bd088be472d3cc
Tomasz <tomasz.iwanicki@onet.pl>
2023-08-12 17:51:59 +0200

    Redirect to /trips when user completes a resevations

--- app/listings/[listingId]/ListingClient.tsx
+++ app/listings/[listingId]/ListingClient.tsx
@@ -69,8 +69,7 @@ const ListingClient: React.FC<ListingClientProps> = ({ listing, currentUser, res
       .then(() => {
         toast.success('Listing reserved!')
         setDateRange(initialDateRange)
-        //Redirent to /trips
-        router.refresh()
+        router.push('/trips')
       })
       .catch(() => {
         toast.error('Something went wrong')
