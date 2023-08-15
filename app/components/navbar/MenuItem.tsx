85c1308c1061455397356d63748c157bfa9e5bc0
Tomasz <tomasz.iwanicki@onet.pl>
2023-08-15 10:50:07 +0200

    Add optional username props

--- app/components/navbar/MenuItem.tsx
+++ app/components/navbar/MenuItem.tsx
@@ -3,15 +3,17 @@
 interface MenuItemProps {
   onClick: () => void
   label: string
+  username?: any
 }
 
-const MenuItem: React.FC<MenuItemProps> = ({ onClick, label }) => {
+const MenuItem: React.FC<MenuItemProps> = ({ onClick, label, username }) => {
   return (
     <div
       onClick={onClick}
       className="px-4 py-3 hover:bg-neutral-100 transition font-semibold"
     >
       {label}
+      <span className="text-rose-500">{username}</span>
     </div>
   )
 }
